72bb8a8abce12c5fb18a4df9a299c311a6059437
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-11-28 13:29:40 -0300

    Screen - cards_found.dart updated 0.1
    Update 0.1.:
    - Added stf class

--- card_searcher/lib/screen/cards_found.dart
+++ card_searcher/lib/screen/cards_found.dart
@@ -7,4 +7,21 @@ Update 0.:
 - Created file
 - Added update log
 
+Update 0.1.:
+- Added stf class
  */
+
+import 'package:flutter/material.dart';
+
+class CardsFound extends StatefulWidget {
+  @override
+  _CardsFoundState createState() => _CardsFoundState();
+}
+
+class _CardsFoundState extends State<CardsFound> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
