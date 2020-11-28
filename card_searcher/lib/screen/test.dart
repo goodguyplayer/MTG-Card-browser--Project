b2d39bfd026f1492f572b40e6ef06cb17502426d
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-11-28 12:45:24 -0300

    Created a test dart to test the API

---
+++ card_searcher/lib/screen/test.dart
@@ -0,0 +1,36 @@
+/*
+Nothing special here, just tests.
+ */
+
+import 'package:card_searcher/models/card_list.dart';
+import 'package:card_searcher/utilities/network_helper.dart';
+import 'package:flutter/material.dart';
+
+class Test extends StatelessWidget {
+  var _dados = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Minhas Requisições"),),
+      body: Text(_dados),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.send),
+        onPressed: () async{
+          var requisicao = NetworkHelper(url:"https://api.magicthegathering.io/v1/cards?name=nissa|jace");
+          var dados = CardList.fromJson(await requisicao.getData());
+
+          for(final e in dados.cardlist){
+            //
+            var currentElement = e;
+            print("Card name.: ${e.name}");
+            print("Card type.: ${e.type}");
+            print("Card image.: ${e.imageUrl}");
+            print("---------------------------------------------------------");
+          }
+
+
+        },
+      ),
+    );
+  }
+}
