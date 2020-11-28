d11708264cabd2b893abec8961832ec5cefa0fb6
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-11-28 13:30:56 -0300

    Screen - card_search.dart created 0
    Update 0.:
    - Created file
    - Added update log
    - Added stless class

---
+++ card_searcher/lib/screen/card_search.dart
@@ -0,0 +1,19 @@
+/*
+Dart screen, exhibits the search screen for the cards
+ */
+
+/*
+Update 0.:
+- Created file
+- Added update log
+- Added stless class
+ */
+
+import 'package:flutter/material.dart';
+
+class CardSearch extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
