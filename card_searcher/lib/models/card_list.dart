26f7bceae8429697b415e6f20ea653ba5822021b
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-11-28 12:28:11 -0300

    models - card_list.dart update 0.1
    Update 0.1.:
    - Added var
    - Added getter and setter
    - Added named constructor - fromJson
    - imported card

--- card_searcher/lib/models/card_list.dart
+++ card_searcher/lib/models/card_list.dart
@@ -7,8 +7,35 @@ Update 0.:
 - Added description
 - Added update log
 - Created class
+
+Update 0.1.:
+- Added var
+- Added getter and setter
+- Added named constructor - fromJson
+- imported card
  */
 
+import 'card.dart';
+
 class CardList{
-  
+
+  // VAR
+  var _cardlist = new List();
+
+  // Getter
+  get cardlist => _cardlist;
+
+  // Setter
+  set cardlist(value) {
+    _cardlist = value;
+  }
+
+  // Named constructor - fromJson
+  CardList.fromJson(Map<String, dynamic> json){
+
+    for (var i=0; i<json['cards'].length; i++) {
+      _cardlist.add(Card.fromJson(json['cards'][i]));
+    }
+
+  }
 }
