fa8a2fc881ad07d9697761180ba0646b482d866a
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-11-28 15:44:40 -0300

    Screen - cards_found.dart updated 0.12
    Update 0.12.:
    - Importing data from card_search.dart
    - Added method getCards

--- card_searcher/lib/screen/cards_found.dart
+++ card_searcher/lib/screen/cards_found.dart
@@ -9,19 +9,47 @@ Update 0.:
 
 Update 0.1.:
 - Added stf class
+
+Update 0.12.:
+- Importing data from card_search.dart
+- Added method getCards
  */
 
+import 'package:card_searcher/models/card_list.dart';
+import 'package:card_searcher/utilities/network_helper.dart';
 import 'package:flutter/material.dart';
 
 class CardsFound extends StatefulWidget {
+  final String text;
+  CardsFound({Key key, @required this.text}) : super(key: key);
+
   @override
   _CardsFoundState createState() => _CardsFoundState();
 }
 
 class _CardsFoundState extends State<CardsFound> {
+  var _cardlist;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text('Encontrados')),
+      body: Center(
+        child: Text(
+          widget.text,
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
   }
+
+
+
+  Future<CardList> getCards(String name) async {
+    var requisicao = NetworkHelper(url:"https://api.magicthegathering.io/v1/cards?name=${name}");
+    var dados = CardList.fromJson(await requisicao.getData());
+  }
+
+
+
 }
