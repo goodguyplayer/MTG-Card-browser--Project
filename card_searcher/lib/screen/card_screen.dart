a5262ca9570b5069b6e19fa41600c7f02f6c2e71
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-11-29 11:57:28 -0300

    Screen - card_screen.dart created

---
+++ card_searcher/lib/screen/card_screen.dart
@@ -0,0 +1,13 @@
+/*
+Dart screen, displays the found card at a higher resolution.
+That is, if the card was found. API can be incomplete and return a null.
+ */
+
+/*
+Update 0.:
+- Created file
+- Added desc of what it is
+- Added update log.
+
+ */
+
