760eea041181ea1af7e02dadf0ca641a960709c7
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-11-29 13:39:00 -0300

    Screen - card_screen.dart update
    Update 0.11.:
    - Added vars for importing data
    - Changed widget to display image

--- card_searcher/lib/screen/card_screen.dart
+++ card_searcher/lib/screen/card_screen.dart
@@ -12,14 +12,37 @@ Update 0.:
 Update 0.1.:
 - Added Stateless widget class
 - Imported packages
+
+Update 0.11.:
+- Added vars for importing data
+- Changed widget to display image
+
  */
 
 import 'package:flutter/material.dart';
 
 class CardScreen extends StatelessWidget {
+  final String imageUrl;
+  CardScreen({Key key, @required this.imageUrl}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView(
+          children: [
+            new Container(
+              child: new Image.network(
+                imageUrl,
+                //height: 60.0,
+                fit: BoxFit.cover,
+              ),
+            ),
+      ],
+            ),
+          ),
+
+        );
   }
 }
 
